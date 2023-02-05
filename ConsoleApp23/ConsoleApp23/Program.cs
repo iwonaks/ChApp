eaaa0648dcc01dcb7d7ec0d12db096c8facee5ef
iwonaks <iwonaks@gmail.com>
2023-02-05 21:59:48 +0100

    homework#1

--- ConsoleApp23/ConsoleApp23/Program.cs
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Iwona";
+bool female = false;
+int age = 15;
+
+if (female==true)
+{
+    if (age>=30)
+    {
+        Console.WriteLine($"Jesteś kobietą o imieniu {name} i masz 30 lat z hakiem ;)");
+    }
+    else
+    {
+        Console.WriteLine($"Jesteś kobietą o imieniu {name} przed trzydziestką.");
+    }
+
+    if(name=="Iwona" && age==46)
+    {
+        Console.WriteLine($"Dokładnie: Kobieta o imieniu {name} w wieku {age} lat.");
+    }
+}
+
+if (female==false)
+{
+    if (age>=18)
+    {
+        Console.WriteLine("Jesteś pełnoletnim mężczyzną.");
+    }
+    else
+    {
+        Console.WriteLine(" Jesteś jeszcze chłopaku niepełnoletni.");
+    }
+}
