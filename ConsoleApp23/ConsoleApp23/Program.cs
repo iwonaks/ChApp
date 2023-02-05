211b36dcdabdf59e4f31ede2e484e438434dd5a8
iwonaks <iwonaks@gmail.com>
2023-02-05 23:36:41 +0100

    homework#2

--- ConsoleApp23/ConsoleApp23/Program.cs
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -1,33 +1,54 @@
-﻿
-string name = "Iwona";
-bool female = false;
-int age = 15;
+﻿int liczba = 488243;
 
-if (female==true)
+string liczbaString = liczba.ToString();
+char [] liczbaTab = liczbaString.ToArray();
+
+int licznik0 = 0;
+int licznik1 = 0;
+int licznik2 = 0;
+int licznik3 = 0;
+int licznik4 = 0;
+int licznik5 = 0;
+int licznik6 = 0;
+int licznik7 = 0;
+int licznik8 = 0;
+int licznik9 = 0;
+
+
+foreach (char c in liczbaTab)
 {
-    if (age>=30)
-    {
-        Console.WriteLine($"Jesteś kobietą o imieniu {name} i masz 30 lat z hakiem ;)");
-    }
-    else
-    {
-        Console.WriteLine($"Jesteś kobietą o imieniu {name} przed trzydziestką.");
-    }
+    if(c=='0')
+    { licznik0++; }
 
-    if(name=="Iwona" && age==46)
-    {
-        Console.WriteLine($"Dokładnie: Kobieta o imieniu {name} w wieku {age} lat.");
-    }
-}
+    if (c=='0')
+    { licznik0++; }
 
-if (female==false)
-{
-    if (age>=18)
-    {
-        Console.WriteLine("Jesteś pełnoletnim mężczyzną.");
-    }
-    else
-    {
-        Console.WriteLine(" Jesteś jeszcze chłopaku niepełnoletni.");
-    }
+    if (c=='1')
+    { licznik1++; }
+
+    if (c=='2')
+    { licznik2++; }
+
+    if (c=='3')
+    { licznik3++; }
+
+    if (c=='4')
+    { licznik4++; }
+
+    if (c=='5')
+    { licznik5++; }
+
+    if (c=='6')
+    { licznik6++; }
+
+    if (c=='7')
+    { licznik7++; }
+
+    if (c=='8')
+    { licznik8++; }
+
+    if (c=='9')
+    { licznik9++; }
 }
+Console.WriteLine("Twoja liczba to:  " + liczba + "\n skałada się z następujących ilości cyf");
+Console.WriteLine("0-->" + licznik0 + "\n" + "1-->" + licznik1 + "\n" +"2-->" + licznik2 + "\n" +"3-->" + licznik3 + "\n" +"4-->" + licznik4 + "\n" +"5-->" + licznik5 + "\n" +"6-->" + licznik6 + "\n" +"7-->" + licznik7 + "\n" +"8-->" + licznik8 + "\n" +"9-->" + licznik9);
