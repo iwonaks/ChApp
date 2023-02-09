a522fca51669292cf58d259c717b376c858227d7
iwonaks <iwonaks@gmail.com>
2023-02-09 01:29:35 +0100

    homework#8

---
+++ ConsoleApp23/ChallengeApp23.Test/TypeTest.cs
@@ -0,0 +1,53 @@
+﻿
+using ConsoleApp23;
+using System.Reflection.Metadata;
+
+namespace ChallengeApp23.Test
+{
+    internal class TypeTest
+    {
+        [Test]
+        public void DiferentInt()
+        {
+            // arage
+            int number1 = 1;
+            int number2 = 2;
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(number2,number1);
+        }
+
+        [Test]
+        public void TheSameString()
+        {
+            // arage
+            string word1 = "Iwona";
+            string word2 = "Iwona";
+
+            // act
+
+            // assert
+            Assert.AreEqual(word1,word2);
+        }
+
+        [Test]
+        public void GetUserShouldReturnDifferentObjects()
+        {
+            // arage
+            var employee1 = GetUser("Iwona", "KS", 46);
+            var employee2 = GetUser("Iwona", "KS", 46);
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(employee1, employee2);
+        }
+
+        private Employee GetUser(string name, string surname, int age) //metoda tworząca nowego użytkownika
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}
